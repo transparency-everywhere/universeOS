2abe1d588d88487e33086472467d9d03e8f5362d
Niczem Olaske <nic@transparency-everywhere.com>
2015-02-16 21:09:45 +0100

    merge conflict in filesystem.js resolved

--- inc/js/filesystem.js
+++ inc/js/filesystem.js
@@ -67,56 +67,43 @@ var filesystem =  new function() {
             favorite = true;
         }      
         var html = '<div class="frameRight fileBrowser_' + folderId + '">';		  			
-<<<<<<< HEAD
-            html += '    <div class="path">';		  			
-            if(is_numeric(folderId)){
-                html += '         universe/' + folders.getPath(folderId);
-            }
-            if(proofLogin() && !empty(folderId)){
-                html += '         <a href=\"#\" id=\"settingsButton\" onclick=\"$(\'.fileBrowserSettings' + folderId + '\').slideToggle(\'slow\'); return false\" title=\"more...\" class=\"btn btn-mini\"><i class=\"glyphicon glyphicon-cog\"></i></a>';
-            }		  			
-            html += '    </div>';		  			
-            html += '    <div class="underFrame" style="overflow: none;">';		  			
-            html += '        <div class="fileBrowser">';		  			
-            if(!empty(folderId) && is_numeric(folderId)){
-                html += '        	<ul class="fileBrowserSettings fileBrowserSettings' + folderId + '">';		  			
-                if(proofLogin()){
-                    html += '                       <li><a href="#" onclick="fav.add(\'folder\', ' + folderId + ')">Fav</a></li>';
-                }		  			
-                if(privacy.authorize(folderData['privacy'], folderData['creator'])){
-                    html += '        		<li><a href="#" onclick="javascript: elements.showCreateElementForm(\'' + folderId + '\');return false">Add Element</a></li>';
-                    html += '                       <li><a href="#" onclick="javascript: folders.showCreateFolderForm(' + folderId + ');return false">Add Folder</a></li>';
-                    html += '        		<li><a href="#" onclick="javascript: popper(\'doit.php?action=addInternLink&parentFolder=' + folderId + '&reload=1\');return false">Add Shortcut</a></li>';
-                }		  			
-                html += '        	</ul>';
-            }
-            if(showFileBrowser){
-                html += this.showFileBrowser(folderId);
-=======
-        html += '    <div class="path">';		  			
-        if(is_numeric(folderId)){
-            html += '         universe/' + folders.getPath(folderId);
-        }
-        if(proofLogin() && !empty(folderId)){
-            html += '         <a href=\"#\" id=\"settingsButton\" onclick=\"$(\'.fileBrowserSettings' + folderId + '\').slideToggle(\'slow\'); return false\" title=\"more...\" class=\"btn btn-mini\">' + this.generateIcon('settings') + '</a>';
-        }		  			
-        html += '    </div>';		  			
-        html += '    <div class="underFrame" style="overflow: none;">';		  			
-        html += '        <div class="fileBrowser">';		  			
-        if(!empty(folderId) && is_numeric(folderId)){
-            html += '        	<ul class="fileBrowserSettings fileBrowserSettings' + folderId + '">';		  			
-            if(proofLogin()){
-                html += '                       <li><a href="#" onclick="fav.add(\'folder\', ' + folderId + ')">Fav</a></li>';
->>>>>>> 0363852dc8217684aa908b3e03817ec767175fb6
-            }		  			
+
+                    html += '    <div class="path">';		  			
+                    if(is_numeric(folderId)){
+                        html += '         universe/' + folders.getPath(folderId);
+                    }
+                    if(proofLogin() && !empty(folderId)){
+                        html += '         <a href=\"#\" id=\"settingsButton\" onclick=\"$(\'.fileBrowserSettings' + folderId + '\').slideToggle(\'slow\'); return false\" title=\"more...\" class=\"btn btn-mini\">' + this.generateIcon('settings') + '</a>';
+                    }		  			
+                    html += '    </div>';		  			
+                    html += '    <div class="underFrame" style="overflow: none;">';		  			
+                    html += '        <div class="fileBrowser">';		  			
+                    if(!empty(folderId) && is_numeric(folderId)){
+                        html += '        	<ul class="fileBrowserSettings fileBrowserSettings' + folderId + '">';		  			
+                        if(proofLogin()){
+                            html += '                       <li><a href="#" onclick="fav.add(\'folder\', ' + folderId + ')">Fav</a></li>';
+                        }		  			
+                        if(privacy.authorize(folderData['privacy'], folderData['creator'])){
+                            html += '        		<li><a href="#" onclick="javascript: elements.showCreateElementForm(\'' + folderId + '\');return false">Add Element</a></li>';
+                            html += '                       <li><a href="#" onclick="javascript: folders.showCreateFolderForm(' + folderId + ');return false">Add Folder</a></li>';
+                            html += '        		<li><a href="#" onclick="javascript: popper(\'doit.php?action=addInternLink&parentFolder=' + folderId + '&reload=1\');return false">Add Shortcut</a></li>';
+                        }		  			
+                        html += '        	</ul>';
+                    }
+                    if(showFileBrowser){
+                        html += this.showFileBrowser(folderId);
+                    }
+        
+        
             if(favorite){
                 html += fav.show(User.userid);
-            }		  			
+            }
             html += '        </div>';	  			
             html += '    </div>';		  			
         html += '    </div>';
         return html;
     };
+
 
     this.showFileBrowser = function(folder){
         var subpath = './';
