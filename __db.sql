42cd245fc81bda5ccaef7b6833f12579ca141582
Niczem Olaske <nic@transparency-everywhere.com>
2015-03-31 05:31:50 +0200

    db.sql fixed

--- __db.sql
+++ __db.sql
@@ -1,8 +1,4 @@
 
---
--- Tabellenstruktur für Tabelle `adminMessages`
---
-
 CREATE TABLE IF NOT EXISTS `adminMessages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` int(11) NOT NULL,
@@ -76,15 +72,17 @@ CREATE TABLE IF NOT EXISTS `elements` (
   `votes` int(11) NOT NULL DEFAULT '0',
   `score` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Daten für Tabelle `elements`
 --
 
 INSERT INTO `elements` (`id`, `title`, `folder`, `creator`, `name`, `year`, `originalTitle`, `language`, `type`, `author`, `license`, `timestamp`, `info1`, `info2`, `info3`, `privacy`, `hidden`, `votes`, `score`) VALUES
-(1, 'myFiles', 4, '', '', '', '', '', 'myFiles', 1, 'Creative Commons Attribution/Share Alike', 1379628108, '', '', '', 'h', 0, 0, 0),
-(2, 'profile pictures', 5, '', '', '', '', '', 'image', 1, 'Creative Commons Attribution/Share Alike', 1379628108, '', '', '', 'p', 0, 0, 0);
+(1, 'myFiles', 0, '', '', '', '', '', 'myFiles', 1, 'Creative Commons Attribution/Share Alike', 1427772136, '', '', '', 'h', 0, 0, 0),
+(2, 'profile pictures', 1, '', '', '', '', '', 'image', 1, 'Creative Commons Attribution/Share Alike', 1427772136, '', '', '', 'p', 0, 0, 0),
+(3, 'myFiles', 4, '', '', '', '', '', 'myFiles', 1, 'Creative Commons Attribution/Share Alike', 1427772434, '', '', '', 'h', 0, 0, 0),
+(4, 'profile pictures', 5, '', '', '', '', '', 'image', 1, 'Creative Commons Attribution/Share Alike', 1427772434, '', '', '', 'p', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -140,7 +138,15 @@ CREATE TABLE IF NOT EXISTS `feed` (
   `votes` int(11) NOT NULL,
   `score` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Daten für Tabelle `feed`
+--
+
+INSERT INTO `feed` (`id`, `author`, `feed`, `timestamp`, `validity`, `type`, `attachedItem`, `attachedItemId`, `privacy`, `votes`, `score`) VALUES
+(1, 1, 'is logged in', 1427772504, 60, 'feed', '', 0, 'p', 0, 0),
+(2, 1, 'is logged in', 1427772516, 60, 'feed', '', 0, 'p', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -186,7 +192,7 @@ CREATE TABLE IF NOT EXISTS `folders` (
   `votes` int(11) NOT NULL DEFAULT '0',
   `score` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Daten für Tabelle `folders`
@@ -196,8 +202,8 @@ INSERT INTO `folders` (`id`, `folder`, `name`, `path`, `creator`, `timestamp`, `
 (1, 0, 'universe', '', 1, 1373158724, 'p;PROTECTED', 0, 0, 0),
 (2, 1, 'userFiles', '/userFiles', 1, 1373158735, 'p;PROTECTED', 0, 2, 0),
 (3, 1, 'groupFiles', '/groupFiles', 1, 1373158745, 'p;PROTECTED;PROTECTED', 0, 16, 8),
-(4, 2, '1', 'upload/userFiles/1', 1, 1379628108, 'h', 0, 0, 0),
-(5, 4, 'userPictures', 'upload/userFiles/1/userPictures', 1, 1379628108, 'h', 0, 0, 0);
+(4, 2, '1', '/Applications/XAMPP/xamppfiles/htdocs/universe/upload/userFiles/1', 1, 1427772434, 'h', 0, 0, 0),
+(5, 4, 'userPictures', '/Applications/XAMPP/xamppfiles/htdocs/universe/upload/userFiles/1/userPictures', 1, 1427772434, 'h', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -337,6 +343,14 @@ CREATE TABLE IF NOT EXISTS `salts` (
   `algo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Daten für Tabelle `salts`
+--
+
+INSERT INTO `salts` (`type`, `itemId`, `receiverType`, `receiverId`, `salt`, `algo`) VALUES
+('auth', 1, 'user', 1, 'U2FsdGVkX19wMWrIn0nboAROK5/xn2I+GBSsOJVypN11OIrJ0zH7G6E0X7zfd5BrI5oVsMgVKbvuEssUweeQrMwKiNcOSX5qTp12bzcWol0Tvsqcw7L9v/bScMfubLExvev+vlpOdJ5sdd1kE1UzVEl2EDnV3cKlsh+NOM54eBAjjb+pDjC3npQaBu2fkAzW8kZLXZZyVxREWGks64m/KQ==', ''),
+('privateKey', 1, 'user', 1, 'U2FsdGVkX1+GO1Dc+yxmIKrijqau2f29mclqXYdbLE7JbMnbz8ZMp+j6eiBZltEydkikXIoK/X5HQFQ7uFSKJx3DjrI6+OQTHQIYpM+ssA0n4b7xxptwOSovA3lmlU0yxeo0F4p5y+k5MkhYTkQW6tipa7STkDMzmQ8RYYu40E5pnYTyAu1EAR63Pb0jOZ3lAUSGAzoSxGOp3L40I3kDeA==', '');
+
 -- --------------------------------------------------------
 
 --
@@ -351,6 +365,13 @@ CREATE TABLE IF NOT EXISTS `signatures` (
   `timestamp` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Daten für Tabelle `signatures`
+--
+
+INSERT INTO `signatures` (`type`, `itemId`, `privateKey`, `publicKey`, `timestamp`) VALUES
+('user', 1, 'U2FsdGVkX18Z1RC9nKBnIX3z20PcHOUdEkcAmkUkqHmFz8zNVf8FlLs6Gun1wj0JAEFb05NzPPljLfJEdjT2Ef7H5vvcJDtSPK3WGmnnwIB8uQQJdVUG2KlEX0WQ6AH1NF+gnigg3UH2LX2q1G/nu57zEGjqvGvM4boftQ/bfCz7r4CxatwKy54mgIR0K1RueJDcI8RfVjtSqNM6q++33jBsQmJf1TjqORxuI3AGNl+9XAtrL/U74oEyqFROHs7JqOxBJ04mEjXurieMSOjz1pqs30y1OHZHJBiqB4VTByj3Vc9IVlT+ZBG6Ozp3XjQbIDE7igZLpjQdETtFd4JHjSny4/oYoT2Ypnz5ZFGPckDzQAaqBY9eQdG4HVW47VZ9A6dpFRXiP6DwccHcKBh/W8g785ZG33lFggd4clTOtT6fiE4MwohIK60E13RWgi196iC7ucygXCy28hhH6THTAVPnEDc4tDnZE887VJ1UuroAoFXlgk7BwDi+YBxa/gdQrVC7ALWgy9XYm6KvGFbpU5+DvqPzs9K9GtkuzAyv2a8GsvN46Rmopj8N7TnpZz+a+TkuoYY7PGn1oZ9s/ew2I+Ip8UAbXKQrfwm3+q9Mk7dtY/+3DdLic3aladjb7GOvdZHF6YBnXPldoK/C2xU3ecQjntCvRbXhcsPaMjs0wxWYstKXUk4Fw6MRX2m38bX3W4QpoNtgfAuKs5HFSRNzxdFI9hSUu36QnYYRcQqF7wlzVFQ7Ssy2Qm4Ot5mP+R9XLiBL3LgcJv8am3zWNdCf1LPB/TC1jcyQKmKDibdkflzzvNyUczM9RbSsr+va3NgkqblLCc39iDKJY7YDnpWuJjoDmxO7aMs+/wrBOIle+bNMYMhnrAdmE73s54KfUjf7sTQEYlt/uBTxyoXJjobG1T+PyUcy8/MjEwcbu7xXw3mCzbR0t/gyuVzxrCo/yISk1cDCRF6MR/YHXH5oT0AVgUyOSjlqPRkk4InHwacyCrujwREJORd07G/mM2E88qHeD81A8bo6kwAGQlI9r7z+ExaXg6Ms2RCHjKoM0mE/RBadHF9YqWb1ygYd2UopgmfUqS6MQlItwAXcm7dJn0cgtXSeIx7lJ588ff3qFD5kS5v6MzCSFbmtv9I8g2jHLnHQpDWbuzEoZGmrErlukUYu7WLfEMC4h0zutFQ7VI3IhNzP51fHoJEcosgtowIH5SG1vrWmAdu3TfAxwv0cKLg9zcg4fm00k4zK5ptzF46lL7+E1ZfCXYHCmBweuyF8jIFuilkVfc7NspdzELXkOB+ol1g78A6ULVXQwvqJlsd0/ewviX6IT88AvY8zDAQzJ7CxbnJVfhYdKIKX+40wRtamCZxpuwE2I5hO6PLgpSziA+GU7c/vQ4DiFwEdxIWOKRUmsVVZsv549MNCu9yqmWVhBcRW6SmluiNHLNDjhvWEoYD0iKZCjh3n6FIiRIPSUBa4YbW22Eq6bWds/6atORDyOD3RHpIo4Yc8bmmZhbYN4lxP3KJCDprAHN3GrlppBXhojNEZ3hbwf5UQNx9gGywVlrcKv08hVIMEp4kUAnsTTFpw09IOLobkBAYZMbAZ0QPIuPrantBGN+XzsEUKTHUO4vbxcd4osWay2HWOpnnK5VntfD0hSFJvhsVGtdEx0Gw02wgPWB5y6mobH+zD+pdazT9Tzn1WBWzrtCd5NzMNu39/pAmKokRqSUxF1v/V87L5U7MITzveZHVwD2RuT89+Pf6xUUdqZxTLK2IeeeeQB6t/oRmK+3CgTShu/MNzZC5Kb+hqQOJVTD887hz6egrqBhkBmE4JiuKBhL8KOz7QrqdV2Mv7mmTtLT1C1fiW41HD5VslqFZHlJYI/tAnmwAXv7UvoqwagBrBdCSwurAmvqU8S7tlpmlbQWyWqwDAU/3lj0GtHcvhtJ5NoSA5gU9RM/hqeCPg9x19kdBk2NMBH9ihKe/vbEhfqEgzMhhrgqp71PoTxffAGcKh94RttNIXKlpq51AkCN1gG8IMDdi6wEZJS8Al4FEb36TpzDM+7u8EzoGbBpv5UT/kY7MJM2FPbqhsPUiU8zVFh3MsngyBQphhs4zkOWy68Qfq1N/Fafc1ccgv0ZWlEwlfe4b/Bt++1SZ5OsDotiZMZQLcQR7aTgDJa2pQ+1wP98AGB653DILXBY8XVeulZ/LemOMKS2lCP8mClok337WKH9yVvam/MpA6lS0Hid0YDd8JkAfZhDydC4Qpq7Evj7ZTmgleRmGXENZVz8czm0FBcg+8T11O6XaSggMpf9eVb0RdTAlbaVmOa1kxzb7wYxUlBFBgxtf5jliGl7wCqaQIFqkrFCtRMv7oxI7qdyUJuJGLZW7V1qVPgIZjnNZGgqDN63+e6Jo+djZ8H3es8Riphs0A6ZgkA8F1+OwY6T8QT454a+1Pw3rvBoCULsSUjXURfx3lIlOBqwQNUbHGXIskCgkSWytSghrZyOVSj+Wv+82JR5tXOlggA8BfhxKnRbKLTXDlb5gurzgxy0PoJn2pyb5CwcMnnZdgDhMdTrUtgicRu4QLwMD59gseBmANhhROCK9/ATXbnplm6zKa8l2Ku5oGlRTnwP4VTpQxuue7oEOB1ydWqAX2DuLtnWZIXKD+SyoAOSZbbtX4yqJnmGg2xgoziyN3nJMxVI9HDcAcgC6HFJ7YtxVgR/6tSIM4g2GQx9JG/e29r3Ta82CvdVt75bm9hOaCu/5EGDbDgX9eu/EZ7N3WtYm0llDNYZJJ5UIJOI1gEp3ZL0JHBzf5unK5b1DNaPmg8eLfH1zktxw3vSTet12TrWmLatR2ubgcOg1g/YVLJxjmSECmXrIdXN49iLumjq2HWb6R+aier7Fp8UOM6tOS4notYi1LDcx2OM7A7KjVtzgetltfdNgJ0jFrFGUcyNaTFhjaHBPhrD4mJI7IwTnMJ1xJOWRT3W7mva9ybOtMvvDYTjvraBEWGLDoiwQjGpdofLeYmY/nQzW8Sh36dVqT3R805/fduaEgycZs3AooyUYiVk3k/uKAn2qlNa48hGJpsmphvQ2zjYgmgSacDVaPK9Otc9yyP5MTrw55xWLrYJmaITuEM4J8Vx7lIGqp/MvDrWJKZSTqmnwyc5BSyQ/+LGfjPQPqJ0XFhtm8K/sq9LNQOHxqrTVRip+YiM2AyZZEn4jnAu2dpPMQhCP1+Vc6S6ObrXby5MfDdU1xb3TId1pguGV7E8/Z4YtNNjqjg+9wp7sDt5KkEtsVRUPxYk4T7DM6wZ7aybPhjzTM6FijqFH5fj71VxxpfWDNJWh21BUXZ1fCZZjaG9taLlZQJwmCdFJcZDHY95LUVGWc51fFHHcUfcwKXIjZiY4IaKLjfdy4ArLsrcBIAWNSnNkessegFi9n5axX2Zq23SoR2Y4eQCZA7yCs8YMg+6b+Bn3UPJfHWW1WWwIQwYaQZnAnt2AZwL33Vb4x55vKFN8HDx0rtSecf+wdZMwRhf3/ea8YEMi/44RODFykM0vxAKU8vHzj+aklb7bpligj3wpkwHABH9jxP7xbil3zt01xHFJokiBegnrh5QygVoE+8FFagOmj0NcMyojxP3oyK18RhQsn5DcghTg3e7rNgpNP4wsNTSLuP3TDNdv1dStvw1SR9OgIR6JCC75cBBg/Xtrscxj5cgk1JFqaT1qovU6x4eSp3eDx6rWu3t9p3PTJhYpdPsn6+IKLEeIr5snYHKP7UA72biWbKpoUGE4YCcBKKiwCazILIDTXZapwWgMdtYPA7pbx2KNNYrd7JgwTkCbYQwArFGP3vZVJ5gbrEpMu5P72AlWvg8uozuuVdRhHmE1ApMGuWDByrfy4Wk8WZfkAlzSiGSJSAfj9GsOl+p1PO97MmdCvVN9GW4IBmtR98P5Z2H4Ix6czBYqBJhQUj3HYNYzTKouHELA4WVsga1PBKshaaNtHi47DKo0dA2XJPTecXvwzC738c0GUbKpPqF0N3mzdVZFxc3abGGLBaLQwPUPLhQF2Xbx7hKfz5g7ifWs4WXnYmhC7Q6CO9JatWvJkAn1Fy79/dpB6cEu47JSF8rHCbUi61nL/xDz/x6QkrkjDNKRW8fFfh5h94W0IY1TfGEJBk5C+j9vxJdO69v/7E62Rj1PE4rlm3XMbnAu/XOORbwmwa1MCvRKSA/f/GzPukzX9NPlTC5tzjzj0+KqdIc0W65ew9l8Xv5D4kid4YhJiaZNy0oGeB9NxBJiWhGzWlvpBgeew8CxTeqlmL+iQokfHjaEMc0UqTOMlc5tU2pMKN7WGyp7vJGOoUh//QXo3vPW1orHSgz7ol6J9VBBDgwDfNQ16bwsN9B2ASWxEFtNmBvY/sSDS', '-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAiJVi54hVUBogEjgaQ3Om\nPbfF/Ve5ptbgqdfn6a/o8v6+74ENbzyKvHPRQAiaBfNKSbvh2CoCjGJqLSms2Jdi\n0ck+ff6gxonav308TfS+yNzEmLk01PyrpGoH3R4gACp6/hPua8FDpLFiwq53UwAV\nhrBcmCO3ihz4OC3TzMyI4pEh28zZRdVr9c44x+JZ/D51PjHc4ubtplWuryrO/hwC\nyz2lV9obhTJQYCpl1xS+fI5vibcDUpBvcjL+xl8jLtgo13/1K1slGiohkPpgFuNL\nFiYUx0DYBzv1J1cosMkx9eUuZNonWtB4lZtV4nFRnvVPieew3ohmzl2UdFRwahIy\nxSCXbPX+CWcJ8bWQF3tSB4y3oEI1Iwcff+beOLmLNVEXpsJeIGfSEl19xrxYgk+S\nZoQZO/BBWF9d+Zht5tvr/4i1BzrUu+r1YUZh0hCrgW9ozs1OD/zhSy7nNGCdhe3n\nAhfFa5EjpPQkxhuN40xcG8BCWXJNK90HwwfNjZXjCZUtyA47Qt1mcHAYbQABHnuu\nbt8iVwpG5qNrZCbocxgT3QEFAr0unJMkIVnvdX+Sm+llsRM8r8CexgQVNSJO4nXi\ni3T7dz/bG7z2eNa3bbbKA8NAgeEkE+6Oytem40rlSK9mm74dKlpNFnETzO+zBOTz\n05VT4SFM9zy7hKz1+vY6gU0CAwEAAQ==\n-----END PUBLIC KEY-----', 1427772434);
+
 -- --------------------------------------------------------
 
 --
@@ -363,7 +384,7 @@ CREATE TABLE IF NOT EXISTS `staticContents` (
   `content` text NOT NULL,
   `comment` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Daten für Tabelle `staticContents`
@@ -439,14 +460,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `buddySuggestions` text NOT NULL COMMENT 'is used to save already suggested users',
   `profile_info` text NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Daten für Tabelle `user`
 --
 
 INSERT INTO `user` (`userid`, `usergroup`, `username`, `password`, `cypher`, `homefolder`, `myFiles`, `profilepictureelement`, `userPicture`, `email`, `regdate`, `lastactivity`, `birthdate`, `realname`, `home`, `place`, `gender`, `school1`, `school2`, `school3`, `university1`, `university2`, `employer`, `typeofwork`, `status`, `openChatWindows`, `priv_activateProfile`, `priv_showProfile`, `priv_profileInformation`, `priv_profilePicture`, `priv_profileFav`, `priv_profileLog`, `priv_activateFeed`, `priv_buddyRequest`, `priv_foreignerMessages`, `priv_foreignerFeeds`, `hash`, `passwordHash`, `backgroundImg`, `startLink`, `buddySuggestions`, `profile_info`) VALUES
-(1, 1, 'admin', '440ac85892ca43ad26d44c7ad9d47d3e', 'md5', 4, 1, 2, '', '1379628108', 1379628318, 1393092205, '', '', '', '', '', '', '', '', '', '', '', '', '', '', 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, '9daac2a5464b45fa16ae355469622277', '', '', 'doit.php?action=showStartMessage', '', '');
+(1, 0, 'admin', '66e5d8ae01617241fb4e66ef7170b1a60c7e6809ef37117dfdbd12808d4824897dfb13e32c8fe5aa403f6105d58dd92346ec84e4d06b63761a6f0bfd28ab9223', 'sha512_2', 4, 3, 4, '', '', 1427772434, 1427772516, '', '', '', '', '', '', '', '', '', '', '', '', '', '', 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 'f65b8301b3cd89fb9873954f12f47134', '', '', 'doit.php?action=showStartMessage', '', '');
 
 -- --------------------------------------------------------
 
@@ -463,15 +484,7 @@ CREATE TABLE IF NOT EXISTS `userGroups` (
   `undeletableFilesystemItems` tinyint(1) NOT NULL DEFAULT '0',
   `editUndeletableFilesystemItems` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Daten für Tabelle `userGroups`
---
-
-INSERT INTO `userGroups` (`id`, `title`, `showAdminPanel`, `protectFileSystemItems`, `editProtectedFilesystemItem`, `undeletableFilesystemItems`, `editUndeletableFilesystemItems`) VALUES
-(1, 'admin', 1, 1, 1, 1, 1),
-(3, 'standard user', 0, 0, 0, 0, 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -492,3 +505,10 @@ CREATE TABLE IF NOT EXISTS `user_privacy_rights` (
   `receive_messages` text NOT NULL,
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `user_privacy_rights`
+--
+
+INSERT INTO `user_privacy_rights` (`userid`, `profile_realname`, `profile_fav`, `profile_files`, `profile_playlists`, `profile_activity`, `buddylist`, `info`, `groups`, `receive_messages`) VALUES
+(1, 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p');
