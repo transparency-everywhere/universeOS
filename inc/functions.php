a1d6070f295b0a4de086366f2654a3e3b49b3aa4
Niczem Olaske <nic@transparency-everywhere.com>
2015-10-05 04:59:13 +0200

    chmod_r added to functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -9,6 +9,37 @@ function filename_safe($name) {
     $except = array('\\', '/', ':', '*', '?', '"', '<', '>', '|'); 
     return str_replace($except, '', $name); 
 }
+
+
+function chmod_R($path, $filemode, $dirmode) { 
+    if (is_dir($path) ) { 
+        if (!chmod($path, $dirmode)) { 
+            $dirmode_str=decoct($dirmode); 
+            print "Failed applying filemode '$dirmode_str' on directory '$path'\n"; 
+            print "  `-> the directory '$path' will be skipped from recursive chmod\n"; 
+            return; 
+        } 
+        $dh = opendir($path); 
+        while (($file = readdir($dh)) !== false) { 
+            if($file != '.' && $file != '..') {  // skip self and parent pointing directories 
+                $fullpath = $path.'/'.$file; 
+                chmod_R($fullpath, $filemode,$dirmode); 
+            } 
+        } 
+        closedir($dh); 
+    } else { 
+        if (is_link($path)) { 
+            print "link '$path' is skipped\n"; 
+            return; 
+        } 
+        if (!chmod($path, $filemode)) { 
+            $filemode_str=decoct($filemode); 
+            print "Failed applying filemode '$filemode_str' on file '$path'\n"; 
+            return; 
+        } 
+    } 
+}
+
   
 function sanitizeText($data){
         // Fix &entity\n;
