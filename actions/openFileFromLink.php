07f75ab7019e5a867d72de6538c093febaff787b
Niczem Olaske <nic@transparency-everywhere.com>
2015-10-27 05:44:45 +0100

    no message

--- actions/openFileFromLink.php
+++ actions/openFileFromLink.php
@@ -4,32 +4,23 @@
 
 
 if(!empty($_GET['openFromLink'])){
-	
 	echo"<script>";
 	switch($_GET['type']){
-		case folder:
-			$folder = $_GET['folder'];
-			echo"openFolder('$folder');";
+		case 'folder':
+			$folder = (int)$_GET['folder'];
+			echo"folders.open('$folder');";
 			break;
-		case element:
-			$element = $_GET['element'];
-                        $classDb = new db();
-                        $elementData = $classDb->select('elements', array('id', mysql_real_escape_string($element)));
-			echo"openElement('$element', '".$elementData['title']."');";
+		case 'element':
+			$element = (int)$_GET['element'];
+			echo"elements.open('$element');";
 			break;
 		case 'file':
-			$file = $_GET['file'];
-			$classDb = new db();
-                        $fileData = $classDb->select('files', array('id', mysql_real_escape_string($file)));
-			echo"openFile('".$fileData['type']."', '$file', '".$fileData['title']."')";
+			$file = (int)$_GET['file'];
+			echo"handlers.files.handler($file);";
 			break;
 		case 'link':
-			$link = $_GET['link'];
-			$classDb = new db();
-                        $linkData = $classDb->select('links', array('id', mysql_real_escape_string($link)));
-			
-			echo"openFile('".$linkData['type']."', '$link', '".$linkData['title']."')";
-			
+			$link = (int)$_GET['link'];
+			echo"handlers.links.handler($link);";
 			break;
 	}
 	echo"</script>";
